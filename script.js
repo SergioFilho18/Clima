387f18b14726d9fa6d8532fcfbfcd549b84672c6
Sérgio Filho <barbosasergiofilho18@gmail.com>
2023-10-24 14:37:05 -0300

    Created the function to make the request at the API URL

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 document.querySelector('.busca').addEventListener('submit',(event)=>{
     event.preventDefault();
+
+    let input = document.querySelector("#searchInput").value;
+
+    if(input !==''){
+        showWarning('Carregando...');
+    }
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=69ee5663f9396bde64e871fd90ab311e&units=metric&lang=pt-br`;
+
 });
+
+function showWarning(msg) {
+    document.querySelector('.aviso').innerHTML = msg;
+}
