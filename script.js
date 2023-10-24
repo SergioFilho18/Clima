332e465ff90ca63350fc0edcb6d9068b189ab5dd
Sérgio Filho <barbosasergiofilho18@gmail.com>
2023-10-24 15:44:30 -0300

    The function of obtaining information from the city about the value has been completed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelector('.busca').addEventListener('submit',(event)=>{
+document.querySelector('.busca').addEventListener('submit', async (event)=>{
     event.preventDefault();
 
     let input = document.querySelector("#searchInput").value;
@@ -8,6 +8,11 @@ document.querySelector('.busca').addEventListener('submit',(event)=>{
     }
 
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=69ee5663f9396bde64e871fd90ab311e&units=metric&lang=pt-br`;
+
+    let results = await fetch(url);
+    let json = await results.json();
+
+    console.log(json);
 
 });
 
